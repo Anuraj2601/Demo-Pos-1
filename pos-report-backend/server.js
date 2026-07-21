c8c6f080b29a74dfdd300baad32422f93c38cd62
anuraj <anurajselvasothy@gmail.com>
2026-07-21 11:07:38 +0530

    changes

--- pos-report-backend/server.js
+++ pos-report-backend/server.js
@@ -191,8 +191,8 @@ app.get("/api/stock", (req, res) => {
 });
 
 
-app.listen(process.env.PORT,()=>{
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on port "+process.env.PORT);
-
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
